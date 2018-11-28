53652a5f631f996085aceb8b00a9f28b6efa3374
Ben Hu <benh@estonetek.com>
2018-11-28 11:15:14 +0800

    Debug serial port changes to ttymxc1

--- include/configs/imx8mq_evk.h
+++ include/configs/imx8mq_evk.h
@@ -115,7 +115,7 @@
 	CONFIG_MFG_ENV_SETTINGS \
 	"script=boot.scr\0" \
 	"image=Image\0" \
-	"console=ttymxc0,115200 earlycon=ec_imx6q,0x30860000,115200\0" \
+	"console=ttymxc1,115200 earlycon=ec_imx6q,0x30860000,115200\0" \
 	"fdt_addr=0x43000000\0"			\
 	"fdt_high=0xffffffffffffffff\0"		\
 	"boot_fdt=try\0" \
@@ -206,8 +206,9 @@
 
 #define CONFIG_BAUDRATE			115200
 
+/*ben(ttymxc1)*/
 #define CONFIG_MXC_UART
-#define CONFIG_MXC_UART_BASE		UART1_BASE_ADDR
+#define CONFIG_MXC_UART_BASE		UART2_BASE_ADDR
 
 /* Monitor Command Prompt */
 #undef CONFIG_SYS_PROMPT
